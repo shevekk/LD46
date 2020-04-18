4fbd38cb6dcdec52be2c80c81ca833faa0732658
Shevekk <maxence.martin@protonmail.com>
2020-04-18 12:16:54 +0200

    Ajout base du code

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject group;
+    public float zPosition = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(group.transform.position.x, group.transform.position.y, zPosition);
+    }
+}
---
+++ Assets/Scripts/GroupScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupScript : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    private float inputX;
+    private float inputY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
+        movement.x = inputX * speed;
+        movement.y = inputY * speed;
+
+        transform.Translate(movement);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+    }
+}
