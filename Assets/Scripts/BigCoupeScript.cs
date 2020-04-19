2e1bbd1158cf18ecc07f5f18d755f40c100e9282
Shevekk <maxence.martin@protonmail.com>
2020-04-20 01:05:56 +0200

    Ajout scene de fin

---
+++ Assets/Scripts/BigCoupeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCoupeScript : MonoBehaviour
+{
+    public Sprite spFlamme;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        FlameScript flame = collision.gameObject.GetComponent<FlameScript>();
+        if (flame != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = spFlamme;
+
+            // Script victoire
+
+        }
+    }
+
+}
